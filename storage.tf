a76d7f765a7f612bea41b7b747aba05bb5ae854c
My <karumanchi.swapna@gmail.com>
2022-03-09 21:22:39 +0530

    terraform

--- storage.tf
+++ storage.tf
@@ -5,7 +5,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket = "terraformpractice"
-    key    = "sample/terraform.tfstate"
+    key    = "terraform.tfstate"
     region = "us-east-1"
   }
 }
